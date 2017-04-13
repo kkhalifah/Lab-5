472b751cc4726862b4dee514823d73624596f944
kkhalifah <karen.khalifah@gmail.com>
2017-04-13 16:40:20 -0400

    first commit

---
+++ namechallenge.js
@@ -0,0 +1,12 @@
+var name1 ="Jenniferr";
+var name2 = " Hunter";
+var name3 = "Karen";
+
+if (name1.length > name2.length && name1.length > name3.length) {
+  console.log(name1 + " has longer name")
+}
+else if (name2.length > name3.length && name2.length > name1.length) {
+  console.log(name2 + "has longer name")
+}
+else { console.log (name3 + " has longer name")
+}
---
+++ script.js
@@ -0,0 +1,14 @@
+console.log("Hi");
+alert("hi");
+
+
+ var ftemp = 32;
+ var ctemp= (ftemp -32)* (5/9);
+ console.log(ctemp);
+
+
+ var ctemp=100;
+ var ftemp = (ctemp*1.8)+32;
+ console.log(ftemp);
+
+ 
